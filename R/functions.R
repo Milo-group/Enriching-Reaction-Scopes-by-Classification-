236264d06662cdd6e3b38cd107617ebdd92d873d
barkais <39986943+barkais@users.noreply.github.com>
2024-12-26 11:17:01 +0200

    Add files via upload

--- R/functions.R
+++ R/functions.R
@@ -729,7 +729,7 @@ ct.plot <- function(class.table, plot.title, conformation) {
     geom_tile(color = 'black', size = 0.75) +  # Tile borders
     coord_fixed() +
     geom_text(aes(label = paste(Freq, "\n", '(', value, '%', ')', sep = '')),
-              size = 3.05, vjust = .5, fontface = "bold", alpha = 1) +  # Text labels on tiles
+              size = 3.35, vjust = .5, fontface = "bold", alpha = 1) +  # Text labels on tiles
     scale_fill_manual(values = c(True = '#6CAE75', False = '#8A0526',
                                  Size = '#FCDEBE', Precision = '#DCAB6B',
                                  Accuracy = '#247BA0'),
